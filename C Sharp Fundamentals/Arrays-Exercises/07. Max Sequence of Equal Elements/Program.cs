30ed8483d1420ceead6ce4a4418483b3ba1b70dc
RadMom <RadMom1111@gmail.com>
2022-08-05 15:50:04 +0300

    C Sharp

---
+++ C Sharp Fundamentals/Arrays-Exercises/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int bestCounter = 1;
+            int counter = 1;
+            int sequenceNum = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] == arr[i])
+                {
+                    counter++;
+
+                    if (counter > bestCounter)
+                    {
+                        bestCounter = counter;
+                        sequenceNum = arr[i];
+                    }
+                }
+                else
+                {
+                    counter = 1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", Enumerable.Repeat(sequenceNum, bestCounter)));
+        }
+    }
+}
