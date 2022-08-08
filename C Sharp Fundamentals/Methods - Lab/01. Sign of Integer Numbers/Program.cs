d8baadc4a4d184c6c18b4bff2937d6b941755a31
RadMom <RadMom1111@gmail.com>
2022-08-08 16:23:58 +0300

    C Sharp

---
+++ C Sharp Fundamentals/Methods - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SignOfIntegerNumbers(int.Parse(Console.ReadLine()));
+        }
+
+        static void SignOfIntegerNumbers(int number)
+        {
+            if (number>0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if(number<0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
---
+++ C Sharp Fundamentals/Methods - Lab/02. Grades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grades(double.Parse(Console.ReadLine()));
+        }
+
+        static void Grades(double grade)
+        {
+            string gradeInWords = string.Empty;
+
+            if (grade >= 2 && grade <= 2.99)
+            {
+                gradeInWords = "Fail";
+            }
+            else if (grade >= 3 && grade <= 3.49)
+            {
+                gradeInWords = "Poor";
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                gradeInWords = "Good";
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                gradeInWords = "Very good";
+            }
+            else if (grade >= 5.50 && grade <= 6)
+            {
+                gradeInWords = "Excellent";
+            }
+            Console.WriteLine(gradeInWords);
+        }
+    }
+}
